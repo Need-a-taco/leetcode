e7ff71f8fb942e33111ffe79566652b0aa33646d
Josh <joshua.zhang@gmail.com>
2024-08-06 12:14:14 -0400

    yee

--- neetcode_roadmap/group_anagrams.py
+++
@@ -1,8 +0,0 @@
-def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
-        res = defaultdict(list)
-        for s in strs: 
-            # Letter by index, frequency by value
-            count = [0] * 26
-            for c in s:
-                count[ord(c) - ord("a")] += 1 
-            res[tuple(count)].append(s) 
